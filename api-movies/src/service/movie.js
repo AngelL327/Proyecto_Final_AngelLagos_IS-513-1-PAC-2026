78ef96572a14018fc23a0a577deca657eb76658a
AngelL327 <ajlagosp@unah.hn>
2026-04-06 10:27:49 -0600

    Se agregaron los metodos update y delete del service / movie.js

--- api-movies/src/service/movie.js
+++ api-movies/src/service/movie.js
@@ -48,7 +48,7 @@ export default class Movie {
 
         return rows
     }
-    
+
     static find = async (id) => {
         const [rows] = await pool.query(
             `${baseSelect}
@@ -58,6 +58,88 @@ export default class Movie {
         )
 
         return rows[0] || null
+    }
+
+    static update = async (id, movie) => {
+        const connection = await pool.getConnection()
+
+        try {
+            await connection.beginTransaction()
+
+            const fields = []
+            const values = []
+
+            if (movie.title !== undefined) {
+                fields.push('title = ?')
+                values.push(movie.title)
+            }
+
+            if (movie.release_year !== undefined) {
+                fields.push('release_year = ?')
+                values.push(movie.release_year)
+            }
+
+            if (movie.synopsis !== undefined) {
+                fields.push('synopsis = ?')
+                values.push(movie.synopsis)
+            }
+
+            if (fields.length > 0) {
+                await connection.query(
+                    `UPDATE movies SET ${fields.join(', ')} WHERE id = ?`,
+                    [...values, id]
+                )
+            }
+
+            if (movie.genres !== undefined) {
+                await connection.query('DELETE FROM movie_genres WHERE movie_id = ?', [id])
+
+                if (movie.genres.length > 0) {
+                    const genreValues = movie.genres.map((genreId) => [id, genreId])
+                    await connection.query(
+                        'INSERT INTO movie_genres(movie_id, genre_id) VALUES ?',
+                        [genreValues]
+                    )
+                }
+            }
+
+            if (movie.directors !== undefined) {
+                await connection.query('DELETE FROM movie_directors WHERE movie_id = ?', [id])
+
+                if (movie.directors.length > 0) {
+                    const directorValues = movie.directors.map((directorId) => [id, directorId])
+                    await connection.query(
+                        'INSERT INTO movie_directors(movie_id, director_id) VALUES ?',
+                        [directorValues]
+                    )
+                }
+            }
+
+            await connection.commit()
+            return await Movie.find(id)
+        } catch (error) {
+            await connection.rollback()
+            throw error
+        } finally {
+            connection.release()
+        }
+    }
+
+    static delete = async (id) => {
+        const connection = await pool.getConnection()
+
+        try {
+            await connection.beginTransaction()
+            await connection.query('DELETE FROM movie_genres WHERE movie_id = ?', [id])
+            await connection.query('DELETE FROM movie_directors WHERE movie_id = ?', [id])
+            await connection.query('DELETE FROM movies WHERE id = ?', [id])
+            await connection.commit()
+        } catch (error) {
+            await connection.rollback()
+            throw error
+        } finally {
+            connection.release()
+        }
     }
 
 
