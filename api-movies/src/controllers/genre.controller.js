dde122731078933df73e3fbbcb4ca92abed1f98d
AngelL327 <ajlagosp@unah.hn>
2026-04-06 10:44:21 -0600

    Se agregaron al controller los metodos de getALL y getBYid

--- api-movies/src/controllers/genre.controller.js
+++ api-movies/src/controllers/genre.controller.js
@@ -1,2 +1,44 @@
 import Genre from '../service/genre.js'
 
+export const getAll = async (req, res) => {
+    try {
+        const genres = await Genre.getAll()
+
+        return res.json({
+            status: 'success',
+            message: 'Listado de generos',
+            data: genres
+        })
+    } catch (e) {
+        return res.status(500).json({
+            status: 'error',
+            message: 'No se pudieron consultar los generos',
+            error: e.message
+        })
+    }
+}
+
+export const getById = async (req, res) => {
+    try {
+        const genre = await Genre.find(req.params.id)
+
+        if (!genre) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'Genero no encontrado'
+            })
+        }
+
+        return res.json({
+            status: 'success',
+            message: 'Genero encontrado',
+            data: genre
+        })
+    } catch (e) {
+        return res.status(500).json({
+            status: 'error',
+            message: 'No se pudo consultar el genero',
+            error: e.message
+        })
+    }
+}
